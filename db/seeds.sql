1e03fe8e6a33bb3810da7d28f27b676137cf90a4
Andrea Ramirez <andrea.anza3@gmail.com>
2023-04-04 15:54:58 -0500

    Fixed seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,27 +4,31 @@ CREATE TABLE department (
 );
 
 INSERT INTO department (name)
-VALUES ('Sales'), ('Enginnering'), ('Finance'), ('Legal');
-
+VALUES ('Sales'), ('Engineering'), ('Finance'), ('Legal');
 
 CREATE TABLE role (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(50) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department (id)
 );
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES (1, 'Sales Lead', 100000, 1), (2, 'Salesperson', 80000, 1), (3, 'Lead Engineer', 150000, 2), (4, 'Software Engineer', 120000, 2), (5, 'Account Manager', 160000, 3), (6, 'Accountant', 125000, 3), (7, 'Legal Team Lead', 250000, 4), (8, 'Lawyer', 190000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUES ( 'Sales Lead', 100000, 1 ), ('Salesperson', 80000, 1), ('Lead Engineer', 150000, 2), ('Software Engineer', 120000, 2), ('Account Manager', 160000, 3), ('Accountant', 125000, 3), ( 'Legal Team Lead', 250000, 4), ('Lawyer', 190000, 4);
+
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT
+    FOREIGN KEY (role_id) REFERENCES role (id),
+    manager_id INT,
+    FOREIGN KEY (manager_id) REFERENCES employee (id)
+    ON DELETE SET NULL
 );
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (1, 'John', 'Doe', 1, null), (2, 'Mike', 'Chan', 2, 1), (3, 'Ashley', 'Rodriguez', 3, null), (4, 'Kevin', 'Tupik', 4, 3), (5, 'Kunal', 'Singh', 5, null), (6, 'Malia', 'Brown', 6, 5), (7, 'Sarah', 'Lourd', 7, null), (8, 'Tom', 'Allen', 8, 7);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('John', 'Doe', 1, null), ('Mike', 'Chan', 2, 1), ('Ashley', 'Rodriguez', 3, null), ('Kevin', 'Tupik', 4, 3), ('Kunal', 'Singh', 5, null), ('Malia', 'Brown', 6, 5), ('Sarah', 'Lourd', 7, null), ('Tom', 'Allen', 8, 7);
 
