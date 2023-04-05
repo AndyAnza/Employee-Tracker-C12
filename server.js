4ba11f43174c9aa7f092ca38195a5b12091db1ed
Andrea Ramirez <andrea.anza3@gmail.com>
2023-04-05 11:29:31 -0500

    Fixed 'View all employees'

--- server.js
+++ server.js
@@ -57,11 +57,11 @@ function choices() {
     });
     //VIEW ALL EMPLOYEES
   } else if (answers.theme === '➡ View All Employees') {
-    db.query('SELECT * FROM employee', (err, results) => {
+    db.query('SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name, role.salary, CONCAT(manager.first_name, \' \', manager.last_name) AS manager_name FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id LEFT JOIN employee AS manager ON employee.manager_id = manager.id', (err, results) => {
       if (err) throw err;
       console.table(results);
       choices();
-    });
+    });  
     //ADD DEPARTMENT
   } else if (answers.theme === '➡ Add Department') {
     const questions = [
